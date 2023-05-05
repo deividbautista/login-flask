94e0213e7cde1882e5b74df237526d938983cd5b
deivid <101598816+deividbautista@users.noreply.github.com>
2023-05-05 10:32:30 -0500

    document

--- src/models/entities/User.py
+++ src/models/entities/User.py
@@ -9,13 +9,14 @@ from flask_login import UserMixin
 class User(UserMixin):
     #Utilizamos el metodo __init__, para poder instanciar la función de manera rapida y facil.
 
-    #Esta función nos brinda las funcionalidad de los atributos del usuario.
+    #Esta función nos brinda la recopilación de los atributos del usuario.
     def __init__(self, id, NDI, password, fullname= "" ) -> None:
         self.id = id
         self.NDI = NDI
         self.password = password
         self.fullname = fullname
 
+    #Para realizar la verificación y comprovación del hash
     @classmethod
     def check_password(self, hashed_password, password):
         return check_password_hash(hashed_password, password)
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,64 +1,47 @@
+
+//-------------------------------------------------------
+//Sección donde definiremos todas las constantes que utiliaremos
+//para brindar la funcionalidad ocultar y ver contraseña.
+//-------------------------------------------------------
+
+//Esto se realiza a travez del query selector, el cual nos permite 
+//selecciónar un elemento de nuestro html y darle funciones.
 const iconEye = document.querySelector('#icon-eye')
 const password = document.querySelector('#password')
 const eye = document.querySelector('#eye')
-const iconEyer = document.querySelector('#icon-eyer')
-const passwordr = document.querySelector('#passwordr')
-const eyer = document.querySelector('#eyer')
-const iconEyer2 = document.querySelector('#icon-eyer2')
-const passwordr2 = document.querySelector('#passwordr2')
-const eyer2 = document.querySelector('#eyer2')
+//-------------------------------------------------------
+//Sección de funcionalidad para ver y ocultar contraseña.
+//-------------------------------------------------------
 
+//Utilizamos la contante definida anteriormente, le brindamos el 
+//evento click, en pocas palabras, cuando se seleccione el objeto 
+//realizara lo siguiente.
 iconEye.addEventListener('click', () =>{
+
+    //Definimos la condicional if, en la que si el tipo del input
+    //es igual a password, tenemos el primer estado en el que al moomento
+    //de dar click se realizara lo que determinemos dentro del if.
     if (password.type === 'password'){
+
+        //Se cambia el tipo del input, por text.
         password.type = 'text'
+
+        //Se remueve la clase, que nos da el icono del ojo cerrado.
         eye.classList.remove('fa-eye-slash')
+
+        //Se añade la clase para el ojo abierto.
         eye.classList.add('fa-eye')
+
+      //En cuyo caso que la condicional no se cumpla, osea que el imput sea
+      //tipo text, se procedera a realizar lo determiinado en el if, en el
+      //momento de seleccionar el elemento.
     } else{
+        //Se cambia el tipo del input, por password.
         password.type = 'password'
+        //Se remueve la clase para el ojo abierto.
         eye.classList.remove('fa-eye')
+        //Se añade la clase para el el icono del ojo cerrado.
         eye.classList.add('fa-eye-slash')
-    }
-
-})
-iconEyer.addEventListener('click', () =>{
-    if (passwordr.type === 'password'){
-        passwordr.type = 'text'
-        eyer.classList.remove('fa-eye-slash')
-        eyer.classList.add('fa-eye')
-    } else{
-        passwordr.type = 'password'
-        eyer.classList.remove('fa-eye')
-        eyer.classList.add('fa-eye-slash')
-    }
-    if (passwordr2.type === 'password'){
-        passwordr2.type = 'text'
-        eyer2.classList.remove('fa-eye-slash')
-        eyer2.classList.add('fa-eye')
-    } else{
-        passwordr2.type = 'password'
-        eyer2.classList.remove('fa-eye')
-        eyer2.classList.add('fa-eye-slash')
-    }
-
-})
-iconEyer2.addEventListener('click', () =>{
-    if (passwordr.type === 'password'){
-        passwordr.type = 'text'
-        eyer.classList.remove('fa-eye-slash')
-        eyer.classList.add('fa-eye')
-    } else{
-        passwordr.type = 'password'
-        eyer.classList.remove('fa-eye')
-        eyer.classList.add('fa-eye-slash')
-    }
-    if (passwordr2.type === 'password'){
-        passwordr2.type = 'text'
-        eyer2.classList.remove('fa-eye-slash')
-        eyer2.classList.add('fa-eye')
-    } else{
-        passwordr2.type = 'password'
-        eyer2.classList.remove('fa-eye')
-        eyer2.classList.add('fa-eye-slash')
     }
 
 })
